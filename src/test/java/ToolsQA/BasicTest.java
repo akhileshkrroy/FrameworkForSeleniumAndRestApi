2e58567db57c5496a80234fa0bd1c1a4a2b42b4d
akhileshkrroy <krroy.akhilesh@gmail.com>
2021-02-01 17:03:19 -0500

    maven project

---
+++ src/test/java/ToolsQA/BasicTest.java
@@ -0,0 +1,19 @@
+package ToolsQA;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BasicTest {
+	
+	@Test
+	public void dumyTest()
+	{
+		Assert.assertFalse(false);
+	}
+	@Test
+	public void dumyTest1()
+	{
+		Assert.assertEquals(1, 1);
+	}
+
+}
